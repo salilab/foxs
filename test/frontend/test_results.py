f77cf5b1ea309c07cc027ca9e1c94293b72d02e0
Ben Webb <ben@salilab.org>
2019-07-29 22:53:08 -0700

    Test display of one PDB+profile, new view

--- test/frontend/test_results.py
+++ test/frontend/test_results.py
@@ -81,6 +81,27 @@ class Tests(saliweb.test.TestCase):
                            re.DOTALL | re.MULTILINE)
             self.assertRegexpMatches(rv.data, r)
 
+    def test_job_one_pdb_profile_new(self):
+        """Test display of job with one PDB, fit to a profile (new view)"""
+        with saliweb.test.make_frontend_job('testjob2') as j:
+            j.make_file('data.txt',
+                        "1abc.pdb test.profile EMAIL 0.50 "
+                        "500 1 1 1 0 0 0 0.00 1.00 3 1\n")
+            j.make_file('inputFiles.txt', "1abc.pdb\n")
+            j.make_file('foxs.log',
+                "1abc.pdb test.profile Chi^2 = 0.202144 c1 = 1.01131 "
+                "c2 = 0.5872 default chi^2 = 0.289123\nsecond line\n")
+            j.make_file('jmoltable.html', "\n")
+
+            c = foxs.app.test_client()
+            rv = c.get('/job/testjob2?passwd=%s' % j.passwd)
+            r = re.compile('PDB files.*Profile file.*User e-mail.*'
+                           '1abc\.pdb.*test\.profile.*EMAIL.*'
+                           'see interactive display\? Use.*old interface.*'
+                           '<canvas id="jsoutput_1".*',
+                           re.DOTALL | re.MULTILINE)
+            self.assertRegexpMatches(rv.data, r)
+
     def test_job_two_pdbs_old(self):
         """Test display of job with two PDBs, no profile (old view)"""
         with saliweb.test.make_frontend_job('testjob2') as j:
