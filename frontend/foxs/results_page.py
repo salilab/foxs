3d593daabf89aa654a2c6b212c3ab39fec9139ed
Ben Webb <ben@salilab.org>
2019-07-29 22:40:22 -0700

    Fix hard-coded links to help page

--- frontend/foxs/results_page.py
+++ frontend/foxs/results_page.py
@@ -1,3 +1,4 @@
+from flask import url_for
 import saliweb.frontend
 import collections
 import os
@@ -29,6 +30,11 @@ class JMolTableReader(object):
                           r'Jmol.setCheckboxGroup(0,[\1])', contents)
         # Add an ID to the info table so we can style it with CSS
         contents = contents.replace('<table ', '<table id="plotcontrol" ')
+        # Fix hard-coded links to help page
+        def help_url(match):
+            return url_for("help", _anchor=match.group(1))
+        contents = re.sub('https:\/\/modbase\.compbio\.ucsf\.edu\/'
+                          'foxs\/help\.html#(\w+)', help_url, contents)
         # Fix links to job results files
         def get_upl(match):
             return self.job.get_results_file_url(match.group(1))
--- test/frontend/test_results.py
+++ test/frontend/test_results.py
@@ -43,7 +43,10 @@ class Tests(saliweb.test.TestCase):
                         "1abc.pdb - EMAIL 0.50 500 1 1 1 0 0 0 0.00 1.00 3 1\n")
             j.make_file('inputFiles.txt', "1abc.pdb\n")
             j.make_file('foxs.log', "\n")
-            j.make_file('jmoltable.html', '<a href="dirname/foo.dat">foo</a>\n')
+            j.make_file('jmoltable.html',
+                        '<a href="dirname/foo.dat">foo</a>\n'
+                        '<a href="https://modbase.compbio.ucsf.edu/'
+                        'foxs/help.html#foo\n')
 
             c = foxs.app.test_client()
             rv = c.get('/job/testjob2?passwd=%s' % j.passwd)
